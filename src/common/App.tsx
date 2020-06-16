758788d3d892b601f547729e54a4f55ac5d97cd1
richardnguyen99 <richard.ng0616@gmail.com>
2020-06-16 10:50:55 -0700

    Added React Helmet for SEO

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -4,16 +4,22 @@
  * @author Richard Nguyen <richard.ng0616@gmail.com>
  */
 import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import { Home, About, User, ErrorPage } from "@common/pages";
 
+import favicon from "^config/assets/favicon.png";
 import "./styles/global.scss";
 import Navbar from "./components/Navbar/Navbar";
 
 const App: React.FC = () => {
   return (
     <>
+      <Helmet
+        defaultTitle="ReactQL - A Server-side-rendered React application"
+        link={[{ rel: "icon", type: "image/png", href: favicon }]}
+      />
       <Navbar>ReactQL</Navbar>
       <Switch>
         <Route exact path="/" component={Home} />
